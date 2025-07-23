a40ad53b5d50555afe1e60f6999e28fcc3c4ce3e
cindy kivala <cindykivala@gmail.com>
2025-07-23 12:46:18 +0300

    useState 1

---
+++ src/pages/FavoriteList.jsx
@@ -0,0 +1,10 @@
+//User's saved games founnd here
+import React, { useState, useEffect } from 'react';
+//consider whether to add css here or wait for collective styling session
+//import "./FavoriteList.css"
+
+//set up our state
+function FavoriteList() {
+    //we want to store the list of fave games
+    const [favorites, setFavorites] = useState([]);
+}
