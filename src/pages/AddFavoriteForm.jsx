ce7496975733da9ae81e858ae31ce4ef86882f2d
iannjunge <ian.njunge@student.moringaschool.com>
2025-07-23 15:30:00 +0300

    Adds changes to AddFavoriteForm

--- src/pages/AddFavoriteForm.jsx
+++ src/pages/AddFavoriteForm.jsx
@@ -9,11 +9,23 @@ function AddFavouriteForm(){
   const [genre, setGenre] = useState(null);
   const [thumbnail, setThumbnail] = useState('');
 
+ //function to handle form submission
+  const handleSubmit = (e)=> {
+  e.preventDefault();
+  
+  }
+
+
   const newGame ={
     title: title,
     genre: genre,
     thumbnail: thumbnail //image url from db.json
  }
+
+ //Add updates (new fav game) to local server
+ fetch("http://localhost:3000/favorites", {method: 'POST'})
+ .then((res)=>res.json)
+ .then((data)=>{onAddFavorite(data)})
 
     return(
         <Form id="Form">
