ef94cf2a7ee94513a0dd566260e8a86e23cde440
RyanWahinya <ryan.wahinya@student.moringaschool.com>
2025-07-24 18:52:58 +0300

    finising touches

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import GameCard from "./components/GameCard";
+// import GameCard from "./components/GameCard";
 import { AuthProvider } from "./components/Authentication";
 
-import GameList from "./pages/GameList";
-import FavoriteList from "./pages/FavoriteList";
-import AddFavoriteForm from "./pages/AddFavoriteForm";
+// import GameList from "./pages/GameList";
+// import FavoriteList from "./pages/FavoriteList";
+// import AddFavoriteForm from "./pages/AddFavoriteForm";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 
@@ -18,12 +18,13 @@ function App() {
         <div className="app">
           <Navbar />
           <Routes>
-            <Route path="/" element={<GameList />} />
-            <Route path="/favorites" element={<FavoriteList />} />
-            <Route path="/add" element={<AddFavoriteForm />} />
+            {/* <Route path="/" element={<GameList />} /> */}
+            {/* <Route path="/favorites" element={<FavoriteList />} /> */}
+            {/* <Route path="/add" element={<AddFavoriteForm />} /> */}
+            <Route path="/" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path="/game/:id" element={<GameCard />} />
+            {/* <Route path="/game/:id" element={<GameCard />} /> */}
             <Route path="*" element={<h2>404 - Page Not Found</h2>} />
           </Routes>
         </div>
